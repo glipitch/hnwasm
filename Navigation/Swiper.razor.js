5767b37e2722af58903b6462ccf48e17d451cf8a
sauropodlet <124493357+sauropodlet@users.noreply.github.com>
2023-03-20 10:24:32 +0000

    Deploying to gh-pages from @ sauropodlet/HnWasm@783d1f3726161772eff0dd1394ef3d16e882c412 🚀

---
+++ Navigation/Swiper.razor.js
@@ -0,0 +1,19 @@
+﻿let startRef;
+let endRef;
+export function attach(dotNetObject) {
+    startRef = function handler(event) {
+        const touches = event.touches[0];
+        dotNetObject.invokeMethod('TouchStart', touches.clientX, touches.clientY);
+    }
+    document.addEventListener("touchstart", startRef);
+
+    endRef = function handler(event) {
+        const changedTouches = event.changedTouches[0];
+        dotNetObject.invokeMethod('TouchEnd', changedTouches.clientX, changedTouches.clientY);
+    }
+    document.addEventListener("touchend", endRef);
+}
+export function detach() {
+    document.removeEventListener("touchstart", startRef);
+    document.removeEventListener("touchend", endRef);
+}
