a4fd0bb0378ab937c76281416d8c235ea8f7dae6
glipitch <124493357+glipitch@users.noreply.github.com>
2026-07-01 15:11:09 +0000

    Deploying to gh-pages from @ glipitch/hnwasm@de974fa0bf66441827fed886a53fe5934f306ede 🚀

--- Navigation/Swiper.razor.js
+++ Navigation/Swiper.razor.js
@@ -1,18 +1,39 @@
-﻿let startRef;
+let startRef;
 let endRef;
-export function attach(dotNetObject) {
+
+export function attach(dotNetObject, sensitivity) {
+    let startX, startY, startTime;
+
     startRef = function handler(event) {
-        const touches = event.touches[0];
-        dotNetObject.invokeMethod('TouchStart', touches.clientX, touches.clientY);
-    }
-    document.addEventListener("touchstart", startRef);
+        const touch = event.touches[0];
+        startX = touch.clientX;
+        startY = touch.clientY;
+        startTime = performance.now();
+    };
 
     endRef = function handler(event) {
-        const changedTouches = event.changedTouches[0];
-        dotNetObject.invokeMethod('TouchEnd', changedTouches.clientX, changedTouches.clientY);
-    }
+        if (startX == null || startY == null || performance.now() - startTime > 300) {
+            startX = startY = null;
+            return;
+        }
+
+        const touch = event.changedTouches[0];
+        const deltaX = startX - touch.clientX;
+        const deltaY = startY - touch.clientY;
+        startX = startY = null;
+
+        if (Math.abs(deltaX) < sensitivity && Math.abs(deltaY) < sensitivity) {
+            return;
+        }
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            dotNetObject.invokeMethodAsync("Swipe", deltaX > 0 ? "rightToLeft" : "leftToRight");
+        }
+    };
+
+    document.addEventListener("touchstart", startRef);
     document.addEventListener("touchend", endRef);
 }
+
 export function detach() {
     document.removeEventListener("touchstart", startRef);
     document.removeEventListener("touchend", endRef);
